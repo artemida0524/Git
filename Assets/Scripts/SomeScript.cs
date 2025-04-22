4c1e75b0e43f00a6f7d4812b6a8fbde452fa819d
artemida0524 <122181904+artemida0524@users.noreply.github.com>
2025-04-22 22:44:24 +0300

    some

--- Assets/Scripts/SomeScript.cs
+++ Assets/Scripts/SomeScript.cs
@@ -1,18 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SomeScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-     Debug.Log("wefewf");   
+        Debug.Log("wefewf");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
     }
 }
