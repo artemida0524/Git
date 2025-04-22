b75e6677105ed5fc9b634d99bb71cdf5a7f8bf63
artemida0524 <122181904+artemida0524@users.noreply.github.com>
2025-04-22 22:38:17 +0300

    Update SomeScript.cs

--- Assets/Scripts/SomeScript.cs
+++ Assets/Scripts/SomeScript.cs
@@ -7,7 +7,7 @@ public class SomeScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+     Debug.Log("wefewf");   
     }
 
     // Update is called once per frame
