a8ea20352d2346abc1c07eb87f53d3f9b7c7805d
artemida0524 <122181904+artemida0524@users.noreply.github.com>
2025-04-25 14:24:54 +0300

    some changes

---
+++ Assets/Scripts/MoveHole.cs
@@ -0,0 +1,76 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEditor.Progress;
+
+public class MoveHole : MonoBehaviour
+{
+    [SerializeField] private MeshFilter meshFilter;
+    [SerializeField] private Transform point;
+    [SerializeField] private MeshCollider meshCollider;
+
+    private Vector3[] vertices;
+    private Vector3[] offsets;
+
+    private void Start()
+    {
+        meshFilter.mesh = Instantiate(meshFilter.sharedMesh);
+        vertices = meshFilter.mesh.vertices;
+        offsets = new Vector3[vertices.Length];
+
+        for (var i = 0; i < vertices.Length; i++)
+        {
+            if (Vector3.Distance(point.position, vertices[i]) < 3)
+            {
+                offsets[i] = vertices[i] - point.position;
+            }
+        }
+    }
+
+    private void Update()
+    {
+
+        Vector3 localPoint = meshFilter.transform.InverseTransformPoint(point.position);
+
+
+        Bounds bounds = meshFilter.mesh.bounds;
+
+        for (var i = 0; i < vertices.Length; i++)
+        {
+            if (offsets[i] != Vector3.zero)
+            {
+
+                Vector3 newPos = localPoint + offsets[i];
+
+                if (bounds.Contains(newPos))
+                {
+                    vertices[i] = newPos;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+
+
+
+
+        foreach (var item in vertices)
+        {
+            if (!bounds.Contains(item))
+            {
+                Debug.Log("fsdfsdfsdfsd");
+            }
+        }
+
+        meshFilter.mesh.vertices = vertices;
+        meshFilter.mesh.RecalculateBounds();
+        meshFilter.mesh.RecalculateNormals();
+        meshFilter.mesh.RecalculateTangents();
+
+        meshCollider.sharedMesh = null;
+        meshCollider.sharedMesh = meshFilter.mesh;
+
+    }
+}
