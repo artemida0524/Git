8113f10a9e883ddbd099e21f797ecfac440d875e
artemida0524 <122181904+artemida0524@users.noreply.github.com>
2025-05-17 17:58:53 +0300

    cahgned some class

--- Assets/Scripts/SomeClassBranch1.cs
+++ Assets/Scripts/SomeClassBranch1.cs
@@ -2,5 +2,9 @@
 
 public class SomeClassBranch1 : MonoBehaviour
 {
-
+    [SerializeField] private int a;
+    [SerializeField] private int b;
+    [SerializeField] private int c;
+    [SerializeField] private int d;
+    [SerializeField] private int e;
 }
